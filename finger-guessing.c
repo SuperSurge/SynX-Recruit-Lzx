06dde4d2da8c3556877e7d754a3b7ad9d50eb0ff
SuperSurge <1643901266@qq.com>
2025-09-28 19:24:50 +0800

    五局三胜

--- finger-guessing.c
+++ finger-guessing.c
@@ -4,33 +4,48 @@
 
 int main()
 {
-    int player, device;
+    int player, computer;
+    int player_score = 0, computer_score = 0;
     srand(time(NULL));
-    printf("猜拳游戏（0：石头、1：剪刀、2：布）\n");
-    while (1)
+
+    printf("=== 猜拳游戏 (五局三胜) ===\n");
+    printf("0-石头, 1-剪刀, 2-布\n");
+
+    while (player_score < 3 && computer_score < 3)
     {
-        printf("请出拳：\n");
+        printf("\n当前比分: 玩家 %d - %d 电脑\n", player_score, computer_score);
+        printf("请出拳: ");
         scanf("%d", &player);
-        device = rand() % 3;
-        if (player != 0 && player != 1 && player != 2)
+
+        computer = rand() % 3;
+        printf("电脑出: %d\n", computer);
+
+        if (player == computer)
         {
-            printf("请正确出拳：0、1、2！\n");
-            continue;
+            printf("平局!\n");
         }
-        printf("玩家：%d vs 电脑：%d\n", player, device);
-        if (player == device)
+        else if ((player == 0 && computer == 1) ||
+                 (player == 1 && computer == 2) ||
+                 (player == 2 && computer == 0))
         {
-            printf("平局\n");
-        }
-        else if ((player == 0 && device == 1) || (player == 1 && device == 2) || (player == 2 && device == 0))
-        {
-            printf("你赢了\n");
-            break;
+            printf("你赢了这一局!\n");
+            player_score++;
         }
         else
         {
-            printf("你输了，请继续\n");
+            printf("你输了这一局!\n");
+            computer_score++;
         }
+    }
+
+    printf("\n最终比分: 玩家 %d - %d 电脑\n", player_score, computer_score);
+    if (player_score > computer_score)
+    {
+        printf("恭喜你获得最终胜利!\n");
+    }
+    else
+    {
+        printf("很遗憾，电脑获胜!\n");
     }
 
     return 0;
